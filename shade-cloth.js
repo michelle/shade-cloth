d0e4598af08d610f20fb386620ab8dd25966180a
Michelle Bu <michelle@stripe.com>
2015-08-25 07:54:02 -0700

    Fix feedback thing

--- shade-cloth.js
+++ shade-cloth.js
@@ -10,10 +10,25 @@ $(function() {
   // Hack to get fullname of current user. If this doesn't work  for every case
   // then we'll need to inject this entire script into the DOM so we can have
   // at the `_gcFullName` global that GH exposes.
-  var _gcFullName = $('#reminder_modal input.email-from.max-width').val().split('<').shift().trim().replace(/"/g, '');
+  //   new method || old method
+  var _gcFullName = $('script[data-key="myFullName"]').data('value');
+  if (!_gcFullName) {
+    // Try old method.
+    var oldMethod = $('#reminder_modal input.email-from.max-width').val();
+    if (oldMethod) {
+      _gcFullName = oldMethod.split('<').shift().trim().replace(/"/g, '');
+    } else {
+      return showFeedback();
+    }
+  }
+
+  var feedback = $('#scorecards .name');
+  if (!feedback.length) {
+    feedback = $('.interview .wrapper .interview-info');
+  }
 
   // An .interview-info class means that feedback has not yet been submitted by someone!
-  $('.interview .wrapper .interview-info').each(function() {
+  feedback.each(function() {
     // Lol Greenhouse globals.
     var $_gcSpan = $(this);
     var _gcNameText = $_gcSpan.text().trim();
@@ -24,6 +39,10 @@ $(function() {
   });
 
   if (_gcShowFeedback) {
+    return showFeedback();
+  }
+
+  function showFeedback() {
     $('.rating-icon.rating-with-name').show();
     $('._gcLlama').remove();
   }
