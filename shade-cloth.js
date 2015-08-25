dc38bc8c2ebcfac043ac286250afc2cca6b232a9
Michelle Bu <michelle@stripe.com>
2015-08-25 08:30:26 -0700

    Fix showing/hiding feedback

--- shade-cloth.js
+++ shade-cloth.js
@@ -1,49 +1,99 @@
 $(function() {
-  // Insert a llama.
-  $('<img>')
-    .attr('src', chrome.extension.getURL('images/llama.png'))
-    .attr('class', '_gcLlama')
-    .attr('width', '15px')
-    .insertAfter('.rating-icon.rating-with-name');
+  var gcShowFeedback = false;
+  var gcForceShowFeedback = false;
 
-  var _gcShowFeedback = true;
-  // Hack to get fullname of current user. If this doesn't work  for every case
-  // then we'll need to inject this entire script into the DOM so we can have
-  // at the `_gcFullName` global that GH exposes.
-  //   new method || old method
-  var _gcFullName = $('script[data-key="myFullName"]').data('value');
-  if (!_gcFullName) {
-    // Try old method.
-    var oldMethod = $('#reminder_modal input.email-from.max-width').val();
-    if (oldMethod) {
-      _gcFullName = oldMethod.split('<').shift().trim().replace(/"/g, '');
+  $('a[href="#scorecard"]').on('click', pollScorecards);
+  pollScorecards();
+
+  function pollScorecards() {
+    var interval = setInterval(function() {
+      if (checkScorecards()) {
+        clearInterval(interval);
+      }
+    }, 500);
+  }
+
+  function checkScorecards() {
+    if ($('.scorecards-section').length === 0) {
+      return false;
+    }
+
+    injectShadeClothScorecardElements();
+
+    // Hack to get fullname of current user. If this doesn't work  for every case
+    // then we'll need to inject this entire script into the DOM so we can have
+    // at the `gcFullName` global that GH exposes.
+    //   new method || old method
+    var gcFullName = $('script[data-key="myFullName"]').data('value');
+    if (!gcFullName) {
+      // Try old method.
+      var oldMethod = $('#reminder_modal input.email-from.max-width').val();
+      if (oldMethod) {
+        gcFullName = oldMethod.split('<').shift().trim().replace(/"/g, '');
+      } else {
+        showFeedback();
+        return true;
+      }
+    }
+
+    var feedback = $('#scorecards .name');
+    if (!feedback.length) {
+      gcShowFeedback = true;
+      $('.interview .wrapper .interview-info').each(function() {
+        // Lol Greenhouse globals.
+        var $gcSpan = $(this);
+        var gcNameText = $gcSpan.text().trim();
+        if (gcNameText && gcNameText.indexOf(gcFullName) === 0) {
+          // If it's me, don't show me feedback yet!
+          gcShowFeedback = false;
+        }
+      });
     } else {
-      return showFeedback();
+      feedback.each(function() {
+        var $gcSpan = $(this);
+        var gcNameText = $gcSpan.text().trim();
+        if (gcNameText && gcNameText.indexOf(gcFullName) === 0) {
+          // If it's me, I've already interviewed this person!
+          gcShowFeedback = true;
+        }
+      });
     }
-  }
 
-  var feedback = $('#scorecards .name');
-  if (!feedback.length) {
-    feedback = $('.interview .wrapper .interview-info');
-  }
-
-  // An .interview-info class means that feedback has not yet been submitted by someone!
-  feedback.each(function() {
-    // Lol Greenhouse globals.
-    var $_gcSpan = $(this);
-    var _gcNameText = $_gcSpan.text().trim();
-    if (_gcNameText && _gcNameText.indexOf(_gcFullName) === 0) {
-      // If it's me, don't show me feedback yet!
-      _gcShowFeedback = false;
+    if (gcShowFeedback) {
+      showFeedback();
     }
-  });
 
-  if (_gcShowFeedback) {
-    return showFeedback();
+    return true;
   }
 
   function showFeedback() {
+    gcForceShowFeedback = true;
     $('.rating-icon.rating-with-name').show();
-    $('._gcLlama').remove();
+    $('.gcLlama').remove();
+  }
+
+  function injectShadeClothScorecardElements() {
+    if (gcShowFeedback || gcForceShowFeedback) {
+      return;
+    }
+
+    if (!$('img.gcLlama').length) {
+      $('<img>')
+        .attr('src', chrome.extension.getURL('images/llama.png'))
+        .attr('class', 'gcLlama')
+        .attr('width', '15px')
+        .insertAfter('.rating-icon.rating-with-name');
+    }
+    if (!$('.gcShowScorecards').length) {
+      var wrapper = $('#scorecards').length ? '#scorecards' : '.interview .wrapper';
+      $('<button>')
+        .attr('class', 'gcShowScorecards')
+        .text('I won\'t be biased; show me the ratings!')
+        .insertAfter('#scorecards')
+        .on('click', function() {
+          showFeedback();
+          $(this).remove();
+        });
+    }
   }
 });
